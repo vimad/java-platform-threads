12924b1e64fbfc701875e43763dbb781d538fefa
vinod <vinod.madubashana@madmobile.com>
2024-12-03 19:04:00 +0530

    exercise_2_2 solution

--- src/main/java/masteringthreads/ch2_basics_of_threads/exercise_2_2/ThreadPoolImpl.java
+++ src/main/java/masteringthreads/ch2_basics_of_threads/exercise_2_2/ThreadPoolImpl.java
@@ -4,6 +4,9 @@ import masteringthreads.ch2_basics_of_threads.exercise_2_1.ThreadPool;
 
 import java.util.*;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Our second exercise is to refactor this code to use ReentrantLock and Condition.
@@ -18,14 +21,10 @@ public class ThreadPoolImpl implements ThreadPool {
     private static final Runnable POISON_PILL = () -> {
     };
 
-    // TODO: Instead of monitor locks (synchronized), use owned locks (ReentrantLock)
-    //  for thread safety and Condition await()/signal() for inter-thread communication.
     private final Deque<Runnable> tasks = new ArrayDeque<>();
 
-    {
-        if (true)
-            throw new UnsupportedOperationException("TODO: Implement with owned locks");
-    }
+    private final Lock lock = new ReentrantLock();
+    private final Condition notEmpty = lock.newCondition();
 
     // This stays the same
     public ThreadPoolImpl(int poolSize) {
@@ -37,46 +36,54 @@ public class ThreadPoolImpl implements ThreadPool {
     }
 
     private Runnable take() throws InterruptedException {
-        // TODO: Replace locking with an owned lock. Remember to use
-        //  try/finally to unlock.
-        synchronized (tasks) {
-            while (tasks.isEmpty()) tasks.wait();
+
+        lock.lock();
+        try {
+            while (tasks.isEmpty()) notEmpty.awaitUninterruptibly();
             Runnable task = tasks.remove();
-            if (task == POISON_PILL) tasks.add(POISON_PILL);
+            if (task == POISON_PILL) {
+                tasks.add(POISON_PILL);
+                notEmpty.signal();
+            }
             return task;
+        } finally {
+            lock.unlock();
         }
     }
 
     @Override
     public void submit(Runnable task) {
         Objects.requireNonNull(task, "task==null");
-        // TODO: Replace locking with an owned lock. We can use signal(),
-        //  as opposed to signalAll(), for all except our POISON_PILL tasks.
-        //  Reason is that we want all our Worker threads to wake up when the
-        //  POISON_PILL is added.
-        synchronized (tasks) {
+        lock.lock();
+        try {
             if (tasks.peekLast() == POISON_PILL)
                 throw new RejectedExecutionException("Pool shut down");
             tasks.add(task);
-            tasks.notifyAll();
+            notEmpty.signal();
+        } finally {
+            lock.unlock();
         }
     }
 
     @Override
     public int getRunQueueLength() {
-        // TODO: Replace locking with an owned lock.
-        synchronized (tasks) {
+        lock.lock();
+        try {
             return (int) tasks.stream()
                     .filter(task -> task != POISON_PILL)
                     .count();
+        } finally {
+            lock.unlock();
         }
     }
 
     @Override
     public void shutdown() {
-        // TODO: Replace locking with an owned lock.
-        synchronized (tasks) {
+        lock.lock();
+        try {
             if (tasks.peekLast() != POISON_PILL) submit(POISON_PILL);
+        } finally {
+            lock.unlock();
         }
     }
 
